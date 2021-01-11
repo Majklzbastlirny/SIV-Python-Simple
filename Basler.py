36920d08e113e0ddefe111fe879ae35b16ab48f9
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2021-01-11 17:25:19 +0100

    Add files via upload

--- Basler.py
+++ Basler.py
@@ -9,14 +9,15 @@ current_time = time.strftime("%H:%M", t)                                        
 print("Zdravím. Jsem pouze jednoduchej script v Pythonu")                                                          # v terminálu napíše věc, co je v závorce
 time.sleep(1)                                                                                                      #počká 1 sekundu
 print("Zatím umím pouze říci čas :-(")                                                                             # v terminálu napíše věc, co je v závorce
-time.sleep(1)                                                                                                      #počká 1 sekundu
-print("Chceš, abych ti řekl čas? Ano/Ne")                                                                          # v terminálu napíše věc, co je v závorce
+time.sleep(1)                                                                                                     #počká 1 sekundu
+print("Chceš, abych ti řekl čas? Ano/Ne/Autor")                                                                    # v terminálu napíše věc, co je v závorce
 otazka = input()                                                                                                   #čeká na odpověď uživatele
 
 if otazka == "Ano":                                                                                                #pokud je odpověď Ano, tak napíše čas do terminálu
     print(current_time)                                                                                            #napíše čas do terminálu
     time.sleep(1)                                                                                                  #počká 1 sekundu
     print("Nyní se ukončím.")                                                                                      # v terminálu napíše věc, co je v závorce
+    time.sleep(5)                                                                                                  #počká 5 vteřin
     exit                                                                                                           #konec
 # komentovaný kód se mi nepodařilo implementovat, protože neumím pracovat s "nested if statement" (neumím česky vyjádřit)
 #    print("Chceš abych ti čas zobrazoval každou vteřinu, nebo se mám ukončit? Ano/Ne")                            # v terminálu napíše věc, co je v závorce
@@ -31,10 +32,12 @@ if otazka == "Ano":                                                             
 
 elif otazka == "Ne":                                                                                               #pokud je odpověď na otázku Ne, tak se ukončí
     print("Ok. Nevadí. Nyní se ukončím")                                                                           # v terminálu napíše věc, co je v závorce
+    time.sleep(5)                                                                                                  #počká 5 vteřin
     exit                                                                                                           # konec
 elif otazka == "Rick":                                                                                             #pokud je odpověd na otázku Rick, tak otevře výchozí prohlížeč a spustí stránku
     print("Oh. Ty jsi našel skrytou část kódu. Well, tohle máš za odměnu ")                                        # v terminálu napíše věc, co je v závorce
     webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')                                                 #otevře se výchozí prohlížeč a spustí stránku
+    time.sleep(5)                                                                                                  #počká 5 vteřin
     exit                                                                                                           #konec
 elif otazka == "Autor":                                                                                            #pokud je odpověď Autor, vypíše se do terminálu text níže
     print(" ")                                                                                                     # v terminálu napíše mezeru
@@ -42,7 +45,10 @@ elif otazka == "Autor":                                                         
     print("Byl jsem vytvořen 5.1.2021")                                                                            # v terminálu napíše věc, co je v závorce
     print("Pokud bude mít líné hovado na mě čas, tak mé aktualizace naleznete zde:" )                              # v terminálu napíše věc, co je v závorce
     print("https://github.com/Majklzbastlirny/SIV-Python-Simple")                                                  # v terminálu napíše věc, co je v závorce
+    time.sleep(10)                                                                                                 #počká 10 vteřin
     exit                                                                                                           #konec
 else:                                                                                                              #nebo
     print("Omlouvám se, ale to nebyla očekávaná odpověď. Nyní se ukončím")                                         # v terminálu napíše věc, co je v závorce
+    time.sleep(3)                                                                                                  #počká 3 vteřin
+
 exit                                                                                                               #konec
